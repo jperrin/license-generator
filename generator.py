490d61eb471488bba8c3815eec38dccafaf799a3
Jim Perrin <Jim.Perrin@microsoft.com>
2021-03-29 12:06:38 -0700

    titles work. need more cleanup

--- generator.py
+++ generator.py
@@ -1,5 +1,9 @@
+import os
+import json
+import glob
 from flask import Flask
 from flask import render_template
+import markovify
 
 generator = Flask(__name__)
 @generator.route('/')
@@ -7,9 +11,22 @@ def home():
     return render_template('index.html')
 
 @generator.route('/build')
-def test():
-    return render_template('index.html')
+def build():
+#    return render_template('index.html')
+    titles = []
+    licensepath = "spdx-license-data/json/details"
+    licensefiles = glob.glob(licensepath + '/*.json')
+
+    for licensefile in licensefiles:
+        with open(licensefile, 'r') as f:
+            json_file = json.load(f)
+            titles.append(json_file['name'])
+
+    modelname = markovify.NewlineText(titles)
+    return modelname.make_sentence()
+
+
 
 
 if __name__ == '__main__':
- generator.run()
+    generator.run()
